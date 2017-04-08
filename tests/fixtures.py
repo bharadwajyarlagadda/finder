355ed3098151a927d92a9b24ac3f57b36891c070
Bharadwaj <yarlagaddabharadwaj@gmail.com>
2017-04-08 02:35:11 +0000

    Add text data for adding to the file created in test fixtures.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -4,6 +4,19 @@ import pytest
 
 parametrize = pytest.mark.parametrize
 
+# Random text retrieved from lorem ipsum generator. For further reference,
+# check http://www.lipsum.com/.
+_TEXT = '''Lorem ipsum dolor sit amet, vestibulum dui dictumst mauris in
+turpis. Neque augue leo, pellentesque purus neque ornare atque, nec vitae,
+tortor justo risus justo. Aptent pellentesque ipsum lacinia lacinia consequat,
+at consectetuer eleifend rhoncus, hendrerit et amet erat, quisque mus sem hac
+donec et.
+
+Leo ut nullam, condimentum in accumsan eu dolor vestibulum, ac egestas
+imperdiet felis amet. Id odio.
+
+'''
+
 
 @pytest.fixture
 def create_temp_folder(tmpdir):
@@ -16,7 +29,7 @@ def create_temp_folder(tmpdir):
 
             for filename in filenames:
                 fp = childdir.join(filename)
-                fp.write('content')
+                fp.write(_TEXT)
 
         return str(parent_dir)
     return _create_temp_folder
@@ -27,6 +40,6 @@ def create_temp_file(tmpdir):
     """Fixture for creating a temporary file."""
     def _create_temp_file(filename):
         fp = tmpdir.mkdir('parent').mkdir('child').join(filename)
-        fp.write('content')
+        fp.write(_TEXT)
         return str(fp)
     return _create_temp_file
