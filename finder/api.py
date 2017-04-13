a714b001737dd61cbf4aa3073fd912fd98655a27
Bharadwaj <yarlagaddabharadwaj@gmail.com>
2017-04-13 04:11:42 +0000

    Implemented Queue() for putting in all the data from the threads

--- finder/api.py
+++ finder/api.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import os
+import queue
 import threading
 import logging as log
 
+from .schema import FinderSchema, DataSchema, ErrorSchema
+from .utils import (
+    split_params,
+    is_kernel_file,
+    is_executable,
+    is_readable,
+    is_image,
+    is_audio,
+    is_video
+)
 
 log.basicConfig(format='%(message)s', level=log.INFO)
+
+lock = threading.Lock
 
 
 class FileReader(threading.Thread):
@@ -17,24 +30,69 @@ class FileReader(threading.Thread):
 
     .. versionadded:: TODO
     """
-    def __init__(self, path=None, pattern=None):
+    def __init__(self, path=None, pattern=None, _queue=None):
         self.path = path
         self.pattern = pattern
+        self._queue = _queue
         threading.Thread.__init__(self)
 
     def run(self):
         """Runs methods for reading the file and searching the pattern in a
         line of text from the file.
         """
-        for line_number, line in read(self.path):
-            if search(line, self.pattern):
-                log.info('{path}:{line_number}: {line}'
-                         .format(path=self.path,
-                                 line_number=line_number,
-                                 line=line))
+        items = []
+        error = []
+        pattern_found = False
+        errors_occurred = False
+
+        try:
+            for line_number, line in read(self.path):
+                if search(line, self.pattern):
+                    items.append(self.set_data(line_number, line))
+                    pattern_found = True
+        except PermissionError as exc:
+            error.append(self.make_error(type=PermissionError,
+                                         message=exc,
+                                         extra=None))
+            errors_occurred = True
+        except OSError as exc:
+            error.append(self.make_error(type='OSError',
+                                         message=exc,
+                                         extra='File might be a kernel file'))
+            errors_occurred = True
+        except UnicodeDecodeError as exc:
+            # TODO: Support all the file encodings.
+            error.append(self.make_error(type='UnicodeDecodeError',
+                                         message=exc,
+                                         extra=None))
+            errors_occurred = True
+
+        if pattern_found or errors_occurred:
+            # Serialize to a JSON-encoded string.
+            response = FinderSchema().dumps({'path': self.path,
+                                             'total_items': len(items),
+                                             'items': items,
+                                             'error': error})
+
+            self._queue.put(response.data)
+
+    def set_data(self, line_number, line):
+        """Returns the serialized data."""
+        data = {'line_number': line_number,
+                'line': line}
+
+        return DataSchema().dump(data).data
+
+    def make_error(self, type, message, extra):
+        """Returns the error serialized data."""
+        error = {'type': type,
+                 'message': message,
+                 'extra': extra}
+
+        return ErrorSchema().dump(error).data
 
 
-def find(paths, pattern=None):
+def find(*paths, pattern=None):
     """Main method for finding the pattern in the given file paths.
 
     Args:
@@ -43,9 +101,15 @@ def find(paths, pattern=None):
 
     .. versionadded:: TODO
     """
-    for path in iterfiles(paths):
-        reader = FileReader(path, pattern)
+    _queue = queue.Queue()
+
+    for path in iterfiles(*paths):
+        reader = FileReader(path, pattern, _queue)
         reader.start()
+        reader.join()
+
+        while not _queue.empty():
+            yield _queue.get()
 
 
 def search(text=None, pattern=None):
@@ -76,7 +140,7 @@ def read(path):
 
     .. versionadded:: TODO
     """
-    with open(path, 'r') as fp:
+    with open(path, 'r', encoding='utf-8') as fp:
         count = 0
 
         for line in fp:
@@ -85,7 +149,7 @@ def read(path):
 
 
 def iterfiles(*paths):
-    """Yields all the file paths in a given directory.
+    """Yields all the non-executable file paths in a given directory.
 
     Args:
         paths (list): List of paths to be walked through.
@@ -98,6 +162,11 @@ def iterfiles(*paths):
     for path in paths:
         path = os.path.expanduser(path) if not os.path.isabs(path) else path
 
+        if not os.path.exists(path):
+            log.info("{path} is not a valid path. Please provide a valid path."
+                     .format(path=path))
+            continue
+
         if os.path.isfile(path) and not is_executable(path):
             yield path
         elif os.path.isdir(path):
@@ -106,5 +175,10 @@ def iterfiles(*paths):
                     file_path = os.path.join(root, file)
 
                     if (os.path.isfile(file_path) and
-                            not is_executable(file_path)):
+                            is_readable(file_path) and
+                            not is_image(file_path) and
+                            not is_executable(file_path) and
+                            not is_kernel_file(file_path) and
+                            not is_audio(file_path) and
+                            not is_video(file_path)):
                         yield file_path
